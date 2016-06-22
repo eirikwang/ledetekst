8b5977f87d2c15fec091b5a6de9691d74927cd2d
martine enger <martine.enger@nav.no>
2016-06-22 15:37:03 +0200

    last commit for today

--- web/src/frontend/app/tekster/tekster-liste.js
+++ web/src/frontend/app/tekster/tekster-liste.js
@@ -5,15 +5,32 @@ import { storeShape } from './../felles/proptype-shapes';
  * "Dum" komponent (i.e.  presentational component) for tekster-modulen(?)
  */
 
+function mapTilHtml(tekstMap){
+    const nokkel = tekstMap[1].nokkel;
+    const spraakObj = tekstMap[1].spraak;
+
+    let htmltekst = `<li>${nokkel}</li>`;
+    htmltekst += `<li>Språk<ul>`;
+    for(let key in spraakObj){
+        let value = spraakObj[key];
+        htmltekst += `<li>${key}: ${value}</li>`;
+    }
+    htmltekst += `</ul></li>`;
+    console.log("Ferdig html for en nøkkel:");
+    console.log(htmltekst);
+    return htmltekst;
+}
+
 function Tekster({ tekster }) {
+    var liste = document.createElement("tekster");
     const teksterElement = Object
         .entries(tekster.data)
-        .map(([key,value]) => <li key={key}>{value}</li>);
-
+        .map((tekstMap) => mapTilHtml(tekstMap));
+    liste.innerHTML = teksterElement;
     return (
         <div>
+            <h1>Ledertekster</h1>
             <ul className="tekster">
-                <h1>Ledertekster</h1>
                 {teksterElement}
             </ul>
         </div>
