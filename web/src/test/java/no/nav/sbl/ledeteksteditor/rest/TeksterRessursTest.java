bbdd2dff100521c303e8160ebfa7aa6788054270
martine enger <martine.enger@nav.no>
2016-06-24 11:03:20 +0200

    updates...

--- web/src/test/java/no/nav/sbl/ledeteksteditor/rest/TeksterRessursTest.java
+++ web/src/test/java/no/nav/sbl/ledeteksteditor/rest/TeksterRessursTest.java
@@ -18,7 +18,7 @@ public class TeksterRessursTest {
         ArrayList<HashMap> result = (ArrayList<HashMap>) ressurs.testTekstHenting().getEntity();
         List<String> nokler = result.stream().map((Map m) -> ((String) m.get("nokkel"))).collect(toList());
         List<Map> spraak = result.stream().map((Map m) -> ((Map) m.get("spraak"))).collect(toList());
-
+        System.out.println(nokler.get(0));
         assertEquals(nokler.get(0), "situasjoner-page.ung-og-uten-erfaring-lenketekst");
         assertThat(spraak.get(0)).hasSize(2);
     }
