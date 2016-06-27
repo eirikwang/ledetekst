796a1893ba9a30d61ce87287ded73d9f6a28ecbd
Caroline Odden <caroline.odden@nav.no>
2016-06-27 08:41:54 +0200

    logginn mappe

---
+++ web/src/frontend/app/logginn/logginn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+const LoggInn = () => (
+    <div>
+        <form action="">
+            <p><input type="text" name="Logg inn" values="" placeholder="Email"/></p>
+        </form>
+    </div>
+)
+
+export default LoggInn
