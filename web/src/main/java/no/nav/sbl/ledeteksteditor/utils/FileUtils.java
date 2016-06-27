c4c2390e2cc5866e92bf3052d678379262192ed0
martine enger <martine.enger@nav.no>
2016-06-27 09:21:22 +0200

    updates

--- web/src/main/java/no/nav/sbl/ledeteksteditor/utils/FileUtils.java
+++ web/src/main/java/no/nav/sbl/ledeteksteditor/utils/FileUtils.java
@@ -1,5 +1,24 @@
 package no.nav.sbl.ledeteksteditor.utils;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
-    
+
+    public static File lagTestMappe(String mappeNavn) {
+        Path homeDir = Paths.get(System.getProperty("user.home"));
+        Path testDir = homeDir.resolve(mappeNavn);
+        System.out.println(testDir.toAbsolutePath());
+        return testDir.toFile();
+    }
+    /*
+    public static File klonTestRepo(String repoURL){
+
+    }
+    */
+    public static void main(String[] args){
+        lagTestMappe("Documents/testMappe");
+
+    }
 }
