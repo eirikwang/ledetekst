f4f0e44d51c1d96265ef515a3bb5646a847756e8
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-06-27 15:15:52 +0200

    La til GitWrapper Exeption som manglet i sist commit

---
+++ web/src/main/java/no/nav/sbl/ledeteksteditor/utils/GitWrapperException.java
@@ -0,0 +1,6 @@
+package no.nav.sbl.ledeteksteditor.utils;
+
+public class GitWrapperException extends Exception{
+    public GitWrapperException() {super();}
+    public GitWrapperException(String message) {super(message);}
+}
