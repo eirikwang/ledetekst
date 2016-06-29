dad1682b0d4d0d6de3fcc8c63a2c3b1b03ad0e45
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-06-29 12:49:12 +0200

    La til exception ved problemer med å skrive til ledetekst fil

---
+++ web/src/main/java/no/nav/sbl/ledeteksteditor/utils/exception/SkrivLedetekstException.java
@@ -0,0 +1,8 @@
+package no.nav.sbl.ledeteksteditor.utils.exception;
+
+public class SkrivLedetekstException extends ApplikasjonsException {
+    public SkrivLedetekstException() {super();}
+    public SkrivLedetekstException(String message) {super(message);}
+    public SkrivLedetekstException(String message, Throwable cause) {super(message, cause);}
+    public SkrivLedetekstException(Throwable cause) {super(cause);}
+}
