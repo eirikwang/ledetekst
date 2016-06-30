cc50c9bb4e17efab773c34bdea899c8b9cb426d6
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-06-30 14:06:10 +0200

    La til test for GitApiException

--- web/src/test/java/no/nav/sbl/ledeteksteditor/rest/GitWrapperTest.java
+++ web/src/test/java/no/nav/sbl/ledeteksteditor/rest/GitWrapperTest.java
@@ -1,6 +1,8 @@
 package no.nav.sbl.ledeteksteditor.rest;
 
+import no.nav.sbl.ledeteksteditor.utils.GitHelper;
 import no.nav.sbl.ledeteksteditor.utils.GitWrapper;
+import no.nav.sbl.ledeteksteditor.utils.exception.ApplikasjonsException;
 import no.nav.sbl.ledeteksteditor.utils.exception.RemoteIkkeFunnetException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
@@ -10,6 +12,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import java.io.File;
+import java.io.IOException;
 
 public class GitWrapperTest {
 
@@ -28,5 +31,11 @@ public class GitWrapperTest {
     public void kasterUnntakVedUgyldigRemote() {
         Repository repo = GitWrapper.getRepo(null, new File("../../invalid_remote_test"));
         GitWrapper.pull(repo);
+    }
+
+    @Test(expected = ApplikasjonsException.class)
+    public void kasterUnntakVedProblemerMedAAPushe() throws IOException {
+        String repoName = GitHelper.createTestRepo();
+        GitWrapper.push(Git.open(new File(repoName)).getRepository());
     }
 }
