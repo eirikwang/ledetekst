c89e0e5a205a719a55d84a49cbf3fdfdaaa790c1
martine enger <martine.enger@nav.no>
2016-06-30 14:26:10 +0200

    Wrote tests for reducer and added actions for success/failure on put call

--- web/src/frontend/app/rediger/rediger-actions.js
+++ web/src/frontend/app/rediger/rediger-actions.js
@@ -1,5 +1,7 @@
 export const FAATT_LEDETEKST = 'FAA_LEDETEKST';
 export const PUT_LEDETEKST = 'PUT_LEDETEKST';
+export const PUTSUCC_LEDETEKST = 'PUTSUCC_LEDETEKST';
+export const PUTFEIL_LEDETEKST = 'PUTFEIL_LEDETEKST';
 
 export function faattLedetekst(nokkel, spraak, innhold) {
     return {
@@ -23,6 +25,20 @@ export function putLedetekst(nokkel, spraak, innhold) {
     };
 }
 
+export function putfeilLedetekst(error) {
+    return {
+        type: PUTFEIL_LEDETEKST,
+        data: error
+    };
+}
+
+export function putsuccLedetekst(data) {
+    return {
+        type: PUTSUCC_LEDETEKST,
+        data
+    };
+}
+
 export function fetchLedetekst(nokkel, spraak, tekst) {
     return dispatch => {
         dispatch(faattLedetekst(nokkel, spraak, tekst));
@@ -42,6 +58,10 @@ export function sendRedigertTekst(nokkel, spraak, tekst, navn, email) {
                 email
             }),
             body: tekst
-        });
+        })
+            .then((response) =>
+                dispatch(putsuccLedetekst(response)))
+            .catch((error) =>
+                dispatch(putfeilLedetekst(error)));
     };
 }
