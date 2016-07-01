0b18cd5867a3bb49a366f9f6a45b36b4a8fb0dd9
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-07-01 12:04:42 +0200

    La til test for at getRepo kaster exception ved problemer med å åpne repo pga io

--- web/src/test/java/no/nav/sbl/ledeteksteditor/rest/GitWrapperTest.java
+++ web/src/test/java/no/nav/sbl/ledeteksteditor/rest/GitWrapperTest.java
@@ -1,7 +1,7 @@
 package no.nav.sbl.ledeteksteditor.rest;
 
-import no.nav.sbl.ledeteksteditor.utils.GitHelper;
 import no.nav.sbl.ledeteksteditor.utils.GitWrapper;
+import no.nav.sbl.ledeteksteditor.utils.exception.AapneRepoException;
 import no.nav.sbl.ledeteksteditor.utils.exception.ApplikasjonsException;
 import no.nav.sbl.ledeteksteditor.utils.exception.RemoteIkkeFunnetException;
 import org.eclipse.jgit.api.Git;
@@ -10,9 +10,12 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
 
 import java.io.File;
 import java.io.IOException;
+
+import static org.mockito.Matchers.any;
 
 public class GitWrapperTest {
 
@@ -28,14 +31,20 @@ public class GitWrapperTest {
     }
 
     @Test(expected = RemoteIkkeFunnetException.class)
-    public void kasterUnntakVedUgyldigRemote() {
+    public void teestGetRepokasterUnntakVedUgyldigRemote() {
         Repository repo = GitWrapper.getRepo(null, new File("../../invalid_remote_test"));
         GitWrapper.pull(repo);
     }
 
     @Test(expected = ApplikasjonsException.class)
-    public void kasterUnntakVedProblemerMedAAPushe() throws IOException {
-        String repoName = GitHelper.createTestRepo();
-        GitWrapper.push(Git.open(new File(repoName)).getRepository());
+    public void testKasterUnntakVedProblemerMedAaPushe() {
+        Repository repo = GitWrapper.getRepo(null, new File("../../invalid_remote_test"));
+        GitWrapper.pull(repo);
+    }
+
+    @Test(expected = AapneRepoException.class)
+    public void testGetRepokasterUnntakVedIOproblemer() throws IOException {
+        PowerMockito.when(Git.open(any(File.class))).thenThrow(new IOException("Kunne ikke åpne fil"));
+        GitWrapper.getRepo(new File(""));
     }
 }
