373e03c7ec423b9100e27be9945f126727e783b1
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-07-04 08:50:51 +0200

    La til uautentisertExceeption for å gi fornuftig tilbakemelding fra API ved manglende ident info i header

--- web/src/main/java/no/nav/sbl/ledeteksteditor/domain/Ident.java
+++ web/src/main/java/no/nav/sbl/ledeteksteditor/domain/Ident.java
@@ -1,10 +1,15 @@
 package no.nav.sbl.ledeteksteditor.domain;
 
+import no.nav.sbl.ledeteksteditor.utils.exception.UautentisertException;
+
 public class Ident {
     public String navn;
     public String epost;
 
     public Ident(String navn, String epost) {
+        if( navn == null || epost == null){
+            throw new UautentisertException("Navn eller epost mangler fra request header");
+        }
         this.navn = navn;
         this.epost = epost;
     }
