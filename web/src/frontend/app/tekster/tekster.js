85ffc80be220b9da80572c195a30c0a07f592c11
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-07-05 12:14:42 +0200

    Byttet ut dobble fnutter med enkle i tekster imports

--- web/src/frontend/app/tekster/tekster.js
+++ web/src/frontend/app/tekster/tekster.js
@@ -1,6 +1,6 @@
-import React, {Component, PropTypes as PT} from "react";
-import {connect} from "react-redux";
-import TeksterListe from "./tekster-liste";
+import React, {Component, PropTypes as PT} from 'react';
+import {connect} from 'react-redux';
+import TeksterListe from './tekster-liste';
 
 function Tekster({ tekster }) {
     return (
