0712e76f740b482a05a694c3970377a7a39cb621
Caroline Odden <caroline.odden@nav.no>
2016-07-06 11:40:41 +0200

    eslint

--- web/src/frontend/app/tekster/tekster-reducer.js
+++ web/src/frontend/app/tekster/tekster-reducer.js
@@ -16,10 +16,11 @@ export default function tekster(state = DEFAULT_STATE, action) {
             return { ...state, status: STATUS.lastet, data: action.data };
         case RECEIVE_FEIL:
             return { ...state, status: STATUS.feilet, data: action.data };
-        case OPPDATER_TEKST:
+        case OPPDATER_TEKST: {
             const newLedetekstState = { ...state.data[action.data.index] };
             newLedetekstState.spraak[action.data.spraak] = action.data.tekst;
             return { ...state, data: [... state.data, ...newLedetekstState] };
+        }
         default:
             return state;
     }
