c340cacab55791fc4d7718e0e7dbd8ae4ea9fe0f
Caroline Odden <caroline.odden@nav.no>
2016-07-06 08:15:58 +0200

    tok vekk rediger-nøkkel-formen fra forsiden

--- web/src/frontend/app/forside/forside.js
+++ web/src/frontend/app/forside/forside.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Tekster from './../tekster/tekster';
 import LoggInn from './../logginn/logginn';
-import Rediger from './../rediger/rediger';
 
 function Forside() {
     return (
