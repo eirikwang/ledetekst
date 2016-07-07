f8256c54c3e7f9e703ccdf9dcaba18ef5eab8dd8
Dag Frode Solberg <dag.frode.solberg@nav.no>
2016-07-07 09:29:25 +0200

    La til redirect tilbake etter logginn

--- web/src/frontend/app/logginn/logginn-actions.js
+++ web/src/frontend/app/logginn/logginn-actions.js
@@ -1,4 +1,5 @@
 import { hentNavnFraEpost, storForbokstavPaaHvertOrd } from './../felles/utils';
+import { goBack } from 'react-router-redux';
 
 export const LOGG_INN = 'LOGG_INN';
 export const EPOST_UGYLDIG = 'EPOST_UGYLDIG';
@@ -16,9 +17,12 @@ function erGyldigEpost(epost) {
 export function loggInn(epost) {
     if (erGyldigEpost(epost)) {
         const navn = storForbokstavPaaHvertOrd(hentNavnFraEpost(epost));
-        return {
-            type: LOGG_INN,
-            data: { navn, epost }
+        return (dispatch) => {
+            dispatch({
+                type: LOGG_INN,
+                data: { navn, epost }
+            });
+            dispatch(goBack());
         };
     }
     return {
