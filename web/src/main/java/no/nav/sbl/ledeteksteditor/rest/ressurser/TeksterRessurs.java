125b026c7d3e60331f09f16bdf2358611eb8fc5a
Nicklas Utgaard <nicklas.utgaard@nav.no>
2016-07-08 15:11:33 +0200

    Raskt oppsett for server-side validering av navn og epost

--- web/src/main/java/no/nav/sbl/ledeteksteditor/rest/ressurser/TeksterRessurs.java
+++ web/src/main/java/no/nav/sbl/ledeteksteditor/rest/ressurser/TeksterRessurs.java
@@ -6,12 +6,15 @@ import no.nav.sbl.ledeteksteditor.domain.Ledetekst;
 import no.nav.sbl.ledeteksteditor.services.LedetekstService;
 import no.nav.sbl.ledeteksteditor.services.LedetekstServiceImpl;
 import no.nav.sbl.ledeteksteditor.utils.exception.UautentisertException;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.File;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -20,6 +23,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Produces(APPLICATION_JSON + ";charset=utf-8")
 @Api(value = "ledetekster", description = "Endpoint for ledetekster")
 public class TeksterRessurs {
+    static final Predicate<String> EPOST = Pattern.compile("^([a-zA-Z].+@nav.no)$").asPredicate();
 
     @Inject
     private LedetekstService ledetekstService;
@@ -37,7 +41,7 @@ public class TeksterRessurs {
     @PUT
     @Path("/{remoteUrl}/{ledetekstnokkel}")
     public Response oppdaterLedetekst(Ledetekst ledetekst, @HeaderParam("navn") String navn, @HeaderParam("epost") String epost, @PathParam("remoteUrl") String remoteUrl) {
-        if( navn == null || epost == null){
+        if (!erAutentisert(navn, epost)) {
             throw new UautentisertException("Navn eller epost mangler fra request header");
         }
 
@@ -46,6 +50,21 @@ public class TeksterRessurs {
                 LedetekstServiceImpl.REPOSITORIES.get(remoteUrl),
                 getRepoDir(remoteUrl), ledetekst, ident);
         return Response.ok(oppdatertLedetekst).build();
+    }
+
+    private boolean erAutentisert(String navn, String epost) {
+        if (erBlank(navn) || erBlank(epost) || !erGyldigEpost(epost)) {
+            return false;
+        }
+        return true;
+    }
+
+    private boolean erGyldigEpost(String epost) {
+        return EPOST.test(epost);
+    }
+
+    private boolean erBlank(String navn) {
+        return navn == null || "undefined".equals(navn) || "null".equals(navn) || StringUtils.isBlank(navn);
     }
 
     @GET
