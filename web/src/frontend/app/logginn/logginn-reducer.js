8e0c5001e9c262a8b8ebbe97550b0e1a55ad092d
martine enger <martine.enger@nav.no>
2016-07-08 14:42:07 +0200

    updates

--- web/src/frontend/app/logginn/logginn-reducer.js
+++ web/src/frontend/app/logginn/logginn-reducer.js
@@ -24,6 +24,7 @@ export default function loggInn(state = DEFAULT_STATE, action) {
                 status: InnloggingsStatus.LOGGET_UT
             };
         case LOGG_UT:
+            localStorage.clear();
             return {
                 ...state,
                 status: InnloggingsStatus.LOGGET_UT,
