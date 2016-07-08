a8e6d0562222dab0e7e2df89cb1e0ec23aead6fb
Eirik L. Wang <eirik.wang@nav.no>
2016-07-08 09:19:40 +0200

    fiksa integrasjonstest

--- web/src/frontend/integration/rest-response.js
+++ web/src/frontend/integration/rest-response.js
@@ -1,4 +1,5 @@
 module.exports = {
+    '/applikasjoner': [{'id': 'test1', 'navn': 'test1', 'url': 'url' }],
     '/test': { 'value': 'This is the response'},
     '/tekster/*': [
         {"spraak":{"nb":"Søk dagpenger","en":"[EN] Søk dagpenger"},"nokkel":"hjelp-til-jobb/hjelp-til-jobb.bruker.illustrasjon.dagpenger"}
