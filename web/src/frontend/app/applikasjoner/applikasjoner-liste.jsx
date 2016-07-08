55632e3afb47f4dc6d81e0e30e592c365b90aefb
Eirik L. Wang <eirik.wang@nav.no>
2016-07-08 13:45:33 +0200

    lagt på styling

--- web/src/frontend/app/applikasjoner/applikasjoner-liste.jsx
+++ web/src/frontend/app/applikasjoner/applikasjoner-liste.jsx
@@ -3,14 +3,17 @@ import { storeShape } from './../felles/proptype-shapes';
 import { Link } from 'react-router';
 function ApplikasjonerListe({ applikasjoner }) {
     const apps = applikasjoner.data.map((app) =>
-        <li key={app.id}>
-            <Link to={`/tekster/${app.id}`}>{app.navn}({app.url})</Link>
+        <li key={app.id} className="">
+            <Link to={`/tekster/${app.id}`} className="panel panel-klikkbart">
+                <h2 className="typo-innholdstittel">{app.navn}</h2>
+                <p className="typo-normal">{app.url}</p>
+            </Link>
         </li>
     );
     return (
         <div>
-            <h1 className="typo-sidetittel text-center">Applikasjoner</h1>
-            <ul>
+            <h1 className="typo-sidetittel text-center blokk-l">Applikasjoner</h1>
+            <ul className="ustilet">
                 {apps}
             </ul>
         </div>
