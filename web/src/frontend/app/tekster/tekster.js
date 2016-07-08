88063d7f3df20f5ebe8defa90a34c38e4f4af3d7
Caroline Odden <caroline.odden@nav.no>
2016-07-08 12:15:18 +0200

    Basic filter

--- web/src/frontend/app/tekster/tekster.js
+++ web/src/frontend/app/tekster/tekster.js
@@ -2,7 +2,17 @@ import React, { PropTypes as PT } from 'react';
 import { connect } from 'react-redux';
 import TeksterListe from './tekster-liste';
 
+function filtrerListe(tekster, search) {
+    if (search !== "") {
+        return tekster.filter(t => t.data.nokkel === search);
+    } else {
+        return tekster;
+    }
+}
+
 function Tekster({ tekster }) {
+    //filtrertListe = filtrerListe(tekster, search);
+    //console.log(filtrertListe);
     return (
         <TeksterListe tekster={tekster} />
     );
