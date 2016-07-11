9346c06ff24f52dc466c1cc4f853c8ca9626b17d
martine enger <martine.enger@nav.no>
2016-07-11 16:35:52 +0200

    fikset alt av styling, la til logginn-knapp

--- web/src/frontend/app/header/header.jsx
+++ web/src/frontend/app/header/header.jsx
@@ -3,32 +3,36 @@ import { connect } from 'react-redux';
 import { loggUt } from './../logginn/logginn-actions';
 import { Link } from 'react-router';
 
-export function Header({ loggInnData, handleClick, pathname }) {
+export function Header({ loggInnData, handleLoggUtClick, pathname }) {
     function hentLoggInnInfo() {
         if (!loggInnData.navn) {
             return (
-                <noscript>Du er ikke logget inn</noscript>
+                <div className="logginn-info">
+                    <button className="knapp knapp-hoved knapp-mini">Logg inn</button>
+                </div>
             );
         }
         return (
             <div className="logginn-info">
-                <span className="typo-undertekst">Du er logget inn som {loggInnData.navn} </span>
-                <button className="knapp knapp-hoved knapp-liten" onClick={handleClick}>Logg ut</button>
+                <span className="typo-undertekst logginn-tekst">{loggInnData.navn}</span>
+                <button className="knapp knapp-hoved knapp-mini" onClick={handleLoggUtClick}>Logg ut</button>
             </div>
         );
     }
 
     function visAppOversiktLink() {
-        if (pathname === '/') {
+        if (pathname === '/' || pathname.indexOf('rediger') > -1 || pathname.indexOf('login') > -1) {
             return (
-                <noscript>Du er allerede på startsiden</noscript>
+                <noscript>Du er allerede på startsiden eller på rediger-siden</noscript>
             );
         }
         return (
-            <div className="til-appoversikt">
-                <img className="pil-tilbake" src="/ledeteksteditor/img/pil-tilbake.svg" alt="Tilbake-ikon" />
-                <Link to="/" className="typo-undertekst hvit-link"> Til applikasjonsoversikt</Link>
-            </div>
+            <Link to="/" className="hvit-link">
+                <div className="til-appoversikt">
+                    <img className="pil-tilbake" src="/ledeteksteditor/img/pil-tilbake.svg" alt="Tilbake-ikon" />
+                    <span className="typo-undertekst tekst-tilappoversikt">Til applikasjonsoversikt</span>
+                </div>
+            </Link>
         );
     }
 
@@ -40,8 +44,8 @@ export function Header({ loggInnData, handleClick, pathname }) {
                         <div className="col-md-12">
                             {visAppOversiktLink()}
                             <div className="app-tittel">
-                                <img className="logo-header" src="/ledeteksteditor/img/hvit-logo.svg" alt="NAV logo" />
-                                <span className="typo-element">Ledeteksteditor</span>
+                                <img className="logo-header" src="/ledeteksteditor/img/logo-nav.svg" alt="NAV logo" />
+                                <span className="typo-element tittel-header">Ledeteksteditor</span>
                             </div>
                             {hentLoggInnInfo(loggInnData)}
                         </div>
@@ -53,14 +57,14 @@ export function Header({ loggInnData, handleClick, pathname }) {
 }
 
 Header.propTypes = {
-    handleClick: PT.func.isRequired,
+    handleLoggUtClick: PT.func.isRequired,
     loggInnData: PT.object.isRequired,
     pathname: PT.string.isRequired
 };
 
 function mapDispatchToProps(dispatch) {
     return {
-        handleClick: () => {
+        handleLoggUtClick: () => {
             dispatch(loggUt());
             window.location.reload();
         }
