7112c40a2c599ebf58dd30e2179aa6ac257fcd20
martine enger <martine.enger@nav.no>
2016-07-12 16:21:30 +0200

    Utnyttet flexbox i stedet for å sette padding og margin manuelt

--- web/src/frontend/app/tekster/teksterlistecontainer/teksterliste/tekster-liste.jsx
+++ web/src/frontend/app/tekster/teksterlistecontainer/teksterliste/tekster-liste.jsx
@@ -1,23 +1,14 @@
 import React, { PropTypes as PT } from 'react';
 import { storeShape } from './../../../felles/proptype-shapes';
-// import RedigerLink from './../redigerlink/redigerlink';
 import { Link } from 'react-router';
 
 function TeksterListe({ tekster, base }) {
     const hentInnholdFor = (ledetekst) =>
-        /*
-        <ul type="disc">
-            {Object.keys(ledetekst.spraak).map((spraak) =>
-                <li key={spraak}>
-                    <RedigerLink base={base} nokkel={ledetekst.nokkel} spraak={spraak} />: {ledetekst.spraak[spraak]}
-                </li>)}
-        </ul>;
-        */
         Object.keys(ledetekst.spraak).map((spraak) =>
             <Link className="tekst-link" to={`${base}/rediger?nokkel=${ledetekst.nokkel}&spraak=${spraak}`}>
                 <div className="tekst-element blokk-xxxs">
                     <img className="tekst-ikon" src="/ledeteksteditor/img/ikon-tekst-copy.svg" alt="Tekst-ikon" />
-                    <span className="typo-avsnitt tekst-innhold">{ledetekst.spraak[spraak]}</span>
+                    <span className="typo-undertekst tekst-innhold">{ledetekst.spraak[spraak]}</span>
                     <span className="typo-undertekst tekst-nokkel">{ledetekst.nokkel}</span>
                     <span className="typo-etikett-liten tekst-spraak">{spraak}</span>
                     <span className="typo-etikett-liten tekst-sistendret">01.01.2000</span>
@@ -25,22 +16,17 @@ function TeksterListe({ tekster, base }) {
             </Link>
         );
 
-
-    const teksterElement = tekster.data.map((ledetekst) =>
-        /*
-        <li key={ledetekst.nokkel} className="blokk-s">
-            <div className="typo-element">{ledetekst.nokkel}</div>
-            Språk
-            {hentInnholdFor(ledetekst)}
-        </li>
-        */
-        hentInnholdFor(ledetekst)
-    );
+    const teksterElement = tekster.data.map((ledetekst) => hentInnholdFor(ledetekst));
 
     return (
         <div>
-            <h1 className="typo-sidetittel text-center">Ledetekster</h1>
             <ul className="tekster">
+                <div className="tittel-element blokk-xxxs">
+                    <span className="typo-etikett-liten tekst-tittel-innhold">Ledetekst</span>
+                    <span className="typo-etikett-liten tekst-tittel-nokkel">Id-nøkkel</span>
+                    <span className="typo-etikett-liten tekst-tittel-spraak">Språk</span>
+                    <span className="typo-etikett-liten tekst-tittel-sistendret">Redigert sist</span>
+                </div>
                 {teksterElement}
             </ul>
         </div>
